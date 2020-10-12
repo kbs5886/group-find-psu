86a4b3437ca00b62e8f68faf96a0ad1a6bec5300
kbs5886 <71106259+kbs5886@users.noreply.github.com>
2020-10-12 13:21:00 -0500

    fixed paths

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json());
 
 if (process.env.NODE_ENV == "production") {
-    app.use(express.status("../client/Dist"));
+    app.use(express.status("./client/Dist"));
 }
 
 app.use("/auth", authRoutes);
