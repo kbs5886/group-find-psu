ffe77f8d178a4230bc255ef52724d2f4c35791d9
Brock Donahue <brockdonahue@Brocks-MacBook-Pro.local>
2020-10-17 05:15:18 -0400

    run it back

--- client/src/components/Home/Main.jsx
+++ client/src/components/Home/Main.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
-import React from 'react'
-import yer from '../../Images/Yer.jpg'
+import React from 'react';
+import yer from '../../Images/Yer.jpg';
 
 function Person({name, description, picture, picDesc}){
     return(
