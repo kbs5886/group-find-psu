d1b73605a5f58a0747b583a4142a2436d3d25800
Kartikey Singh <kbs5886@psu.edu>
2020-10-17 14:03:59 -0500

    fix for react router issues

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 
 if (process.env.NODE_ENV == "production") {
-    app.use(express.static("./client/build"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "/client/index.html"));
-    });
+    // app.use(express.static("./client/build"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, "/client/index.html"));
+    // });
 }
 
 app.listen(PORT, () => {
