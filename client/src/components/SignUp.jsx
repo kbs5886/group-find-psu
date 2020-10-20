f6a4ed48fb0bf7acc2eb8bb7c36d6b8ac8dcacd1
Kartikey Singh <kbs5886@psu.edu>
2020-10-19 23:13:29 -0500

    fixed useEffect import issue

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import signupIllus from "../Images/signup_illus.svg";
 import { useHistory } from "react-router-dom";
