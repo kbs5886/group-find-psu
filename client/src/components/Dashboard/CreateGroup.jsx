a97b9d6ce1d4a682924ed050586e2532d4d7654e
Kartikey Singh <kartikeysingh@Kartikeys-Air.attlocal.net>
2020-12-04 17:03:58 -0600

    fixed forms error

--- client/src/components/Dashboard/CreateGroup.jsx
+++ client/src/components/Dashboard/CreateGroup.jsx
@@ -11,7 +11,9 @@ const CreateGroup = (props) => {
     const [requirements, setRequirements] = useState("");
     const [contact, setContact] = useState("");
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
         axios.post(
             "/group/create",
             { name, tags, category, requirements, contact },
@@ -40,7 +42,7 @@ const CreateGroup = (props) => {
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                <form action="" onSubmit={() => handleSubmit()}>
+                                <form onSubmit={(e) => handleSubmit(e)}>
                                     {/*header*/}
                                     <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
                                         <h3 className="text-3xl font-semibold">
@@ -133,6 +135,7 @@ const CreateGroup = (props) => {
                                     </div>
                                     <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
                                         <button
+                                        
                                             className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
                                             type="button"
                                             style={{
